67094db9b7b06f3c4560d29ee6519bee3176beca
Ash Wilson <ashley.wilson2@student.anglia.ac.uk>
2016-09-30 12:05:22 +0100

    Consensus Algorithm

---
+++ OOPw1CA.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class OOPw1CA
+{
+public OOPw1CA()
+    {
+
+    }
+public void Run()
+{
+            string username = "Ashley";
+            Console.WriteLine("Hello World!" + username);
+}
+
+}
+
--- Program.cs
+++ Program.cs
@@ -4,10 +4,22 @@ namespace ConsoleApplication
 {
     public class Program
     {
+       public static OOPw1CA app = new OOPw1CA();
+      
         public static void Main(string[] args)
         {
-            string username = "Ashley";
-            Console.WriteLine("Hello World! + username");
+           int Icecream = ReadInt("Please choose a flavour by pressing 1, 2 or 3: Chocolate = 1, Strawberry = 2, Vanilla = 3");
+           
+
         }
+
+        public static int ReadInt(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            return Convert.ToInt32(input);
+
+        }
+
     }
 }
